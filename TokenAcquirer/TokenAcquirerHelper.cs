a10c3665e74d7f9ee898228572434a463230b36b
ARUNKUMAR S <arunkumar.ak1197@outlook.com>
2022-07-13 12:26:14 +0530

    Add project files.

---
+++ TokenAcquirer/TokenAcquirerHelper.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Identity.Client;
+using Microsoft.Identity.Web;
+using System;
+using System.Threading.Tasks;
+
+namespace TokenAcquirer
+{
+    public interface ITokenAcquirerHelper
+    {
+        Task<AuthenticationResult> GetTokenAsync();
+    }
+    public class TokenAcquirerHelper : ITokenAcquirerHelper
+    {
+        private static readonly string ClientId = "";
+        private static readonly string TenantId = "";
+        private static readonly string ClientSecret = "";
+        private static readonly string Instance = "https://login.microsoftonline.com/{0}";
+
+        public async Task<AuthenticationResult> GetTokenAsync()
+        {
+            // Congiguration as per the flow you are using
+            var config = new AuthenticationConfig
+            {
+                ClientId = ClientId,
+                Tenant = TenantId,
+                ClientSecret = ClientSecret,
+                Instance = Instance
+            };
+
+
+            IConfidentialClientApplication app;
+
+            // Building the client application
+            app = ConfidentialClientApplicationBuilder.Create(config.ClientId)
+                .WithClientSecret(config.ClientSecret)
+                .WithAuthority(new Uri(config.Authority))
+                .Build();
+
+            // Setting in-memory cache
+            app.AddInMemoryTokenCache();
+
+            // Setting scopes
+            string[] scopes = new string[] { "https://graph.microsoft.com/.default" };
+
+            // Acquiring token
+            AuthenticationResult result = await app.AcquireTokenForClient(scopes).ExecuteAsync();
+            return result;
+        }
+    }
+}
---
+++ TokenRequester/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TokenAcquirer;
+
+namespace TokenRequester
+{
+    internal class Program
+    {
+        private static readonly ITokenAcquirerHelper _tokenAcquirer;
+        static Program() => _tokenAcquirer = new TokenAcquirerHelper();
+        static void Main()
+        {
+
+            GetTokenFirstTime();
+            GetTokenSubsequently();
+            Console.ReadLine();
+        }
+
+        private static void GetTokenFirstTime()
+        {
+            Console.WriteLine("Acquiring a token for the first time...\n");
+            var result = _tokenAcquirer.GetTokenAsync().GetAwaiter().GetResult();
+            Console.WriteLine($"Token source: {result.AuthenticationResultMetadata.TokenSource}\n");
+            Console.WriteLine($"Access Token:\n\n{result.AccessToken}");
+
+        }
+
+        private static void GetTokenSubsequently()
+        {
+            Console.WriteLine("\n\nAcquiring a token subsequent time...\n");
+            var result = _tokenAcquirer.GetTokenAsync().GetAwaiter().GetResult();
+            Console.WriteLine($"Token source: {result.AuthenticationResultMetadata.TokenSource}\n");
+            Console.WriteLine($"Access Token:\n\n{result.AccessToken}");
+        }
+    }
+}
